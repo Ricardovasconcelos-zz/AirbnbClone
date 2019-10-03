006e689d08f0050cf90720f8bd71e32f7c2e377f
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-10-03 15:00:47 -0300

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 require('./database/database')
 
 const server = express()
+server.use(cors())
 server.use(express.json())
 
 server.use(routes)
