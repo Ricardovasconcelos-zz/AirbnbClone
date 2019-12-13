adb848a10f3a9e8b483bc67227c945aaaf1d8655
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2019-12-12 22:21:27 -0300

    config image web

--- backend/src/model/Spot.js
+++ backend/src/model/Spot.js
@@ -1,30 +1,32 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
-const Spot = new Schema({
-    thumbnail:String,
+const Spot = new Schema(
+  {
+    thumbnail: String,
     title: String,
-    price:Number,
-    city:String,
-    itens:[String],
+    price: Number,
+    city: String,
+    itens: [String],
     user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User"
     }
-    
-}, {
+  },
+  {
     toJSON: {
-        virtuals:true
+      virtuals: true
     }
+  }
+);
+
+//TO SHOW IMAGE IN MOBILE
+Spot.virtual("thumbnail_url").get(function() {
+  return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
-//Para Imagem no WEB
-// Spot.virtual('thumbnail_url').get(function(){
-//     return `http://localhost:3333/files/${this.thumbnail}`
-// })
+//TO SHOW IMAGE IN MOBILE
+// Spot.virtual("thumbnail_url").get(function() {
+//   return `http://192.168.0.7:3333/files/${this.thumbnail}`;
+// });
 
-//Para Imagem no MOBILE
-Spot.virtual('thumbnail_url').get(function(){
-    return `http://192.168.0.7:3333/files/${this.thumbnail}`
-})
-
-module.exports = model('Spot', Spot)
+module.exports = model("Spot", Spot);
